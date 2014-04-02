d9ecfb82ab8ee6e44245ac8139a6a45c7603ee11
jczetta <jczetta@gmail.com>
2014-04-01 23:14:12 -0400

    cleanup amara_langs, todo cleanup print/write data

--- amara_langs.py
+++ amara_langs.py
@@ -32,40 +32,6 @@ class AmaraInfoSet(object):
 				self.non_eng_langs.append(k)
 
 #BELOW OLD CODE ABOVE NEW CODE
-
-## http://www.amara.org/api2/partners/videos/7AO3TZh2WGOK/languages/?format=json
-
-baseurl = "http://www.amara.org/api2/partners/videos/{}/languages/?format=json" # needs videoid
-
-f = open("OM_amara_ids_2013-12-18.txt", "r") # regularly need to run other file, open correct file (easy if run today, etc, date)
-ids = [x.strip() for x in f.readlines()]
-lang_names = []
-langs = {}
-
-
-
-
-
-total_langs = 0
-#transls = 0
-for i in ids:
-	t = json.loads(requests.get(baseurl.format(i)).text)
-	total_langs += int(t["meta"]["total_count"])
-	#transls += int(t["meta"]["total_count"])
-	for ob in t["objects"]:
-		if ob["language_code"]:
-			if ob["language_code"] in langs:
-				langs[ob["language_code"]] += 1
-			else:
-				langs[ob["language_code"]] = 1
-		if ob["name"] != "english":
-			lang_names.append(ob["name"])
-
-			#langs.get(ob["language_code"],0) + 1
-			# if ob["language_code"] == "en":
-			# 	transls -= 1
-
-
 ## here's stuff where I was printing data ad-hoc-ily
 
 print "lang dict",langs
