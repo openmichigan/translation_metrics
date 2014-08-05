3ed5150a5df359fc9948f4224a87fb8380a71748
jczetta <jczetta@gmail.com>
2014-08-05 18:40:56 -0400

    fixed double counting, write new files

--- amara_langs.py
+++ amara_langs.py
@@ -5,64 +5,76 @@ import datetime
 
 
 # parse iana subtag (language code) registry
-stf = open("iana_subtag_registry.txt").read()
-langlist = stf.split("%%")
-codelangs = {}
-
-separated = [x.strip().split("\n") for x in langlist]
-for si in separated:
-	for term in si:
-		if term.startswith("Subtag:"):
-			#print term.split(":")[1].strip().rstrip()
-			#print si[2].split(":")[1].strip().rstrip()
-			codelangs[term.split(":")[1].strip().rstrip()] = si[2].split(":")[1].strip().rstrip()
 
 
 # remainder: objects, code using the objects
 class AmaraInfoSet(object):
 	"""Object gets/stores metrics for videos/translations across whatever boundary/set specified by account(s) chosen"""
-	def __init__(self,relvid): #relvid should be a RelevantVideos instance
+	def __init__(self,relv): #relvid should be a RelevantVideos instance
+		# manage and create langmap
+		stf = open("iana_subtag_registry.txt").read()
+		langlist = stf.split("%%")
+		codelangs = {}
+
+		separated = [x.strip().split("\n") for x in langlist]
+		for si in separated:
+			for term in si:
+				if term.startswith("Subtag:"):
+					#print term.split(":")[1].strip().rstrip()
+					#print si[2].split(":")[1].strip().rstrip()
+					codelangs[term.split(":")[1].strip().rstrip()] = si[2].split(":")[1].strip().rstrip()
+		relvid = relv
 		relvid.manage_links()
 		self.baseurl = "http://www.amara.org/api2/partners/videos/{}/languages/?format=json" # structured for .format arg
 		self.vid_ids = relvid.ids.keys()
+		self.flag = False
 		#print self.vid_ids
 		self.total_indiv_subtitles = 0
 		self.lang_names = []
 		self.langs = {}
-		self.get_info()
+		#self.get_info()
 		self.get_non_english_langs()
-		self.lang_map = codelangs # created in py file, above
+		self.lang_map = codelangs # created dict above
 		self.lang_map['swa'] = "Swahili" # because of a multiple version problem, see error correction
-		self.lang_map['zh'] = "Chinese" # same deal
+		self.lang_map['zh'] = "Chinese" # same thing
+		self.lang_map['zhx'] = "Chinese" # same thing
 
 	def get_info(self):
+		#print self.lang_map
+		self.flag = True # notes that get info has been run on this instance
+		now = datetime.datetime.now()
+		ft = open("video_ids_langs_{}.csv".format(now), "w")
+		ft.write("Video ID,Language Translation\n")
 		for i in self.vid_ids:
-			try:
-				t = json.loads(requests.get(self.baseurl.format(i)).text)
-				self.total_indiv_subtitles += int(t["meta"]["total_count"])
-				for ob in t["objects"]:
-					if ob["language_code"]:
-						# if ob["language_code"] == "zh":
-						# 	ob["language_code"] = "zhx" # trying to correct for inconsistency in lang codes
-						if ob["language_code"] in self.langs:
-							self.langs[ob["language_code"]] += 1
-						else:
-							self.langs[ob["language_code"]] = 1
-							# if "zh" in ob["language_code"]:
-							# 	print "yes zh"
-					if ob["name"] != "english": # using for non-english languages, primary use case
-						self.lang_names.append(ob["name"])
-			except Exception, e:
-				print "Exception raised - {}".format(e)
-				print "Looking at video: {}".format(i)
-				continue
+			if i != "openmichigan":
+				try:
+					t = json.loads(requests.get(self.baseurl.format(i)).text)
+					self.total_indiv_subtitles += int(t["meta"]["total_count"]) # weird number TODO check
+					for ob in t["objects"]:
+						if ob["language_code"]:
+							#print ob["language_code"] # DEBUG
+							lc = ob["language_code"]
+							if "-" in lc:
+								lc = lc.split("-")[0].replace("-","").strip().rstrip()
+							if self.lang_map[lc] in self.langs:
+								self.langs[self.lang_map[lc]] += 1
+							else:
+								self.langs[self.lang_map[lc]] = 1
+							ft.write("{}\t{}\n".format(i,self.lang_map[lc]))
+						if ob["name"] != "english": # using for non-english languages, primary use case
+							self.lang_names.append(ob["name"]) 
+
+				except Exception, e:
+					print "Exception raised - {}".format(e)
+					print "Looking at video: {}".format(i)
+					continue
 
 	def get_total_subtitles(self):
 		"""Gets and returns the number of total non-English subtitles extant in this InfoSet"""
 		tot = 0
-		for i in self.langsnums:
+		for i in self.langs:
 			if "en" not in i:
-				tot += self.langsnums[i]
+				tot += self.langs[i]
 		return tot
 
 	def get_non_english_langs(self):
@@ -75,42 +87,16 @@ class AmaraInfoSet(object):
 				#print self.langs[k]
 				self.non_eng_langs.append(k)
 
-	def prep_info(self):
-		self.get_info()
-		#self.get_non_english_langs() # not necessary, but can include
-		self.langsnums = {}
-		lks = []
-		# appending all the correct language code keys to lks list
-		for k in self.langs.keys():
-			if "-" in k:
-				k = k.split("-")[0]
-			lks.append(k.replace("-","").strip().rstrip())
-		# constructing reference dict
-		for i in lks:
-			if i in self.lang_map and i in self.langs:
-				if self.lang_map[i] in self.langsnums:
-					self.langsnums[self.lang_map[i]] += self.langs[i]
-				else:
-					self.langsnums[self.lang_map[i]] = self.langs[i]
-			elif "zh" in i: # correcting for discrepancy in language code list being used
-				if "Chinese" in self.langsnums:
-					self.langsnums["Chinese"] += 1
-				else:
-					self.langsnums["Chinese"] = 1 
-			else:
-				print "ERROR FOUND - cannot decode language code: {}\n".format(i) 
-# TODO double check number correctness
-
 	def __str__(self):
 		"""Provides print-to-console representation of InfoSet"""
-		self.prep_info()
+		self.get_info()
 		s = """
 Number of total languages including English: {}
 Total non-English translations: {}
 Languages:\n
 """.format(len(self.langs.keys()),self.total_transls) #len([x for x in self.langs.keys() if "english" not in x.lower()])
-		for l in sorted(self.langsnums.keys(),key=lambda x:self.langsnums[x]):
-			s += "- {} {}\n".format(self.langsnums[l], l)
+		for l in sorted(self.langs.keys(),key=lambda x:self.langs[x]):
+			s += "- {} {}\n".format(self.langs[l], l)
 		return s
 	
 	def __repr__(self):
@@ -118,17 +104,23 @@ Languages:\n
 		pass
 
 	def write_tsv(self):
-		self.prep_info()
+		#self.get_info()
+		if not self.flag:
+			self.get_info()
 		now = str(datetime.datetime.now())
 		f = open("amara_info_{}.csv".format(now), "w") # creates csv file dated with current date/time
 		f.write("Language Name\tNumber of Subtitles\n")
 		f.write("Total Subtitles (Including English)\t{}\n".format(self.total_indiv_subtitles))
 		f.write("Total Non-English Subtitles\t{}\n".format(int(self.get_total_subtitles()))) # TODO add the total number of non-English subtitles
-		for l in sorted(self.langsnums.keys(),key=lambda x:self.langsnums[x]):
-			f.write("{}\t{}\n".format(l,self.langsnums[l]))
+		for l in sorted(self.langs.keys(),key=lambda x:self.langs[x]):
+			f.write("{}\t{}\n".format(l,self.langs[l]))
 
 if __name__ == '__main__':
 	# Code here for testing / Open.Michigan use case(s)
+
+	# TODO 2 files:  language, # videos || videoid, language
+	# TODO: code here for OM use case, -- if certain arg, don't do OM stuff, otherwise do OM + kludewig
+	# TODO: commit to translation_metrics repository + comment cleanup
 
 	# access accounts to be used and save in AmaraAccount instances -- here, Open.Michigan's
 	om_acct = av.AmaraAccount("openmichigan.video")
@@ -138,5 +130,7 @@ if __name__ == '__main__':
 	# create an AmaraInfoSet instance and write file/print information to console
 	# not yet writing by course because of association problem, but theoretically that object inst would work
 	total_info = AmaraInfoSet(relvs)
-	total_info.write_tsv()
 	print total_info 
+	total_info.write_tsv()
+
+	relvs.write_file()
